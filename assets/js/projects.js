b598d6aabee24cedcb07b278ba60adf9344049c2
Undefined <baokhang4930@gmail.com>
2024-05-20 16:35:16 +0700

    Update projects.js

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -14,24 +14,32 @@ fetch('./projects.json')
 
     // Create project items
     Object.entries(data).forEach(([category, projects]) => {
-      projects.forEach(project => {
-        const projectItem = document.createElement('li');
-        projectItem.className = 'project-item';
-        projectItem.dataset.filterItem = category;
-        projectItem.innerHTML = `
-          <a href="#">
-            <figure class="project-img">
-              <div class="project-item-icon-box">
-                <ion-icon name="eye-outline"></ion-icon>
-              </div>
-              <img src="./assets/images/${project.image}" alt="${project.title}" loading="lazy">
-            </figure>
-            <h3 class="project-title">${project.title}</h3>
-            <p class="project-category">${category}</p>
-          </a>
-        `;
-        projectList.appendChild(projectItem);
-      });
+     projects.forEach(project => {
+       const projectItem = document.createElement('li');
+       projectItem.className = 'project-item active';
+       projectItem.dataset.filterItem = category;
+       projectItem.innerHTML = `
+         <a href="#">
+           <figure class="project-img">
+             <div class="project-item-icon-box">
+               <ion-icon name="eye-outline"></ion-icon>
+             </div>
+             <img src="./assets/images/${project.image}" alt="${project.title}" loading="lazy">
+           </figure>
+           <h3 class="project-title">${project.title}</h3>
+           <p class="project-category">${category}</p>
+         </a>
+       `;
+       projectList.appendChild(projectItem);
+     });
+   });
+
+   const selectList = document.querySelector('.select-list');
+    Object.keys(data).forEach(category => {
+      const selectItem = document.createElement('li');
+      selectItem.className = 'select-item';
+      selectItem.innerHTML = `<button data-select-item="${category}">${category}</button>`;
+      selectList.appendChild(selectItem);
     });
   })
  .catch(error => console.error('Error:', error));
